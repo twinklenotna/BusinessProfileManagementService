9fab5960f3e6f851b06c61ecfbb9765e07eb8970
Twinkle <twinkle@linkedin.com>
2023-08-26 19:39:10 +0530

    added logs

--- src/main/java/com/example/BusinessProfileManagement/kafka/ProfileUpdateRequestConsumer.java
+++ src/main/java/com/example/BusinessProfileManagement/kafka/ProfileUpdateRequestConsumer.java
@@ -4,12 +4,15 @@ import com.example.BusinessProfileManagement.model.BusinessProfileRequest;
 import com.example.BusinessProfileManagement.service.ProfileRequestService;
 import com.example.BusinessProfileManagement.service.ProfileValidationService;
 import com.example.BusinessProfileManagement.service.requestState.BusinessProfileRequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProfileUpdateRequestConsumer {
+  Logger logger = LoggerFactory.getLogger(ProfileUpdateRequestConsumer.class);
   final BusinessProfileRequestContext _businessProfileRequestContext;
 
   public ProfileUpdateRequestConsumer(ProfileValidationService profileValidationService,
@@ -19,6 +22,7 @@ public class ProfileUpdateRequestConsumer {
 
   @KafkaListener(topics = "profileRequest", groupId = "profileRequestValidationGroup")
   public void consumeProfileUpdateRequest(BusinessProfileRequest request) {
+    logger.debug("Processing request with requestId: {}  and profileId: {} ",request.getRequestId(),request.getProfileId());
     _businessProfileRequestContext.processRequest(request);
   }
 }
